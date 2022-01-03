a1970198f8ef9114a1bff745f432f3f30af45e77
“[atefeh <[atefeh@buzzjective.com]>
2022-01-03 10:14:15 +0330

    First setup and add Vuetify, create components and import it!

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="6">
+      <div></div>
+      <h3>About Us</h3>
+      <p>
+        Ninjacart is India's largest Fresh Produce Supply Chain Company. We are
+        pioneers in solving one of the toughest supply chain problems of the
+        world by leveraging innovative technology. We source fresh produce from
+        farmers and deliver them to businesses within 12 hours.
+      </p>
+      <v-btn>Know more</v-btn>
+    </v-col>
+    <v-col cols="12" md="6">
+      <div class="logo">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+      <div class="list-view">
+          <v-img :src="require('@/assets/images/listView.png')"></v-img>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "AboutUs",
+
+  data: () => ({}),
+};
+</script>
---
+++ src/components/BannerInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row class="text-center">
+    <v-col cols="12" md="6">
+      <h3>
+        <span>Revolutionizing the Fresh Produce Supply Chain</span>
+      </h3>
+      <h6>
+        Driven by cutting edge technology, we source fresh produce from farmers
+        and sell to businesses, across India
+      </h6>
+      <v-btn>Learn More</v-btn>
+    </v-col >
+    <v-col cols="12" md="6">
+      <v-img :src="require('@/assets/images/download.jpeg')"></v-img>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "BannerInfo",
+
+  data: () => ({
+  }),
+};
+</script>
